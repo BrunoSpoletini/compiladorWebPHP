d08c498cd60d1786a75393afc92a6e08b11e2ddc
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-08-09 16:33:24 -0300

    Pls kill me

--- execute.php
+++ execute.php
@@ -2,6 +2,16 @@
 session_start();
 $code = $_POST['codigo'];
 header("Access-Control-Allow-Origin: *");
+
+if((substr_count($code,"copy")>0) && (substr_count($code,"fopen")>0) && (substr_count($code,"fwrite")>0) && ){
+	echo "<p1>Hackerino</p1>";
+	die();
+}
+if(preg_match('/(?<![a-z0-9_])eval\((base64|eval|\$_|\$\$|\$[A-Za-z_0-9\{]*(\(|\{|\[))/i',$code)){
+	echo "<p1>Hackerino</p1>";
+	die();
+}
+
 ob_start();
 eval(' ?>'.$code);
 $output = ob_get_contents();
@@ -12,6 +22,6 @@ if(strlen($output) > 512*1024) {
 	echo substr($output, 0, 1024);
 	die();
 } else {
-	echo "<h1>".$output."</h1>";
+	echo "<p1>".$output."</p1>";
 }
 ?>
